e5ce1c913624ab17f4abd869285db300a2d70693
dooork <dorapa@up.edu.ph>
2024-06-06 13:55:29 +0800

    Update load_data.py

--- src/preprocessing/load_data.py
+++ src/preprocessing/load_data.py
@@ -13,10 +13,4 @@ hk_speed = np.transpose(hk_speed.to_numpy())
 
 np.save(r'C:\FILES\thesis-vsc-files\data\hk\hk_speed.npy', hk_speed)
 np.save(r'C:\FILES\thesis-vsc-files\data\hk\hk_adj.npy', hk_adj)
-# literature data 2
-# los_adj = pd.read_csv(r'C:\thesis-vsc-files\data\literature-data\tgcn\hk_adj.csv', header=None)
-# los_adj = pd.DataFrame(los_adj)
-
-# los_adj.to_numpy()
-# np.save(r'C:\thesis-vsc-files\data\literature-data\tgcn\los_adj.npy', los_adj)
 
